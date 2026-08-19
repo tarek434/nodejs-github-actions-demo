8607506c348df6580af0d63ff03b90a34bf87653
tarek434 <mdtarekkhan434@gmail.com>
2026-08-19 23:43:22 +0600

    Configure ESLint for Node.js and browser

--- eslint.config.js
+++ eslint.config.js
@@ -2,14 +2,34 @@ const js = require("@eslint/js");
 
 module.exports = [
   js.configs.recommended,
+
+  // Node.js backend
   {
-    files: ["src/**/*.js"],
+    files: ["src/index.js"],
     languageOptions: {
       ecmaVersion: "latest",
-      sourceType: "commonjs"
-    },
-    rules: {
-      "no-console": "off"
+      sourceType: "commonjs",
+      globals: {
+        process: "readonly",
+        __dirname: "readonly",
+        console: "readonly"
+      }
+    }
+  },
+
+  // Browser frontend
+  {
+    files: ["src/public/js/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "script",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        fetch: "readonly",
+        setInterval: "readonly"
+      }
     }
   }
 ];
